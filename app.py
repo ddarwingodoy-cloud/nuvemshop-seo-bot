1196df5cd33614aa868a161e74eac9cc55699be2
ddarwingodoy-cloud <ddarwingodoy@gmail.com>
2026-03-28 19:38:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +11,22 @@ def home():
 @app.route("/callback")
 def callback():
     code = request.args.get("code")
-    return f"Callback recebido. Code: {code}"
+
+    if not code:
+        return "Code não encontrado"
+
+    url = "https://www.tiendanube.com/apps/authorize/token"
+
+    payload = {
+        "client_id": os.environ.get("NUVEMSHOP_APP_ID"),
+        "client_secret": os.environ.get("NUVEMSHOP_CLIENT_SECRET"),
+        "grant_type": "authorization_code",
+        "code": code
+    }
+
+    response = requests.post(url, json=payload)
+
+    return f"Resposta da API: {response.text}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
